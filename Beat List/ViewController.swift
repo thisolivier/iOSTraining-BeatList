e91d4ff14689f61fc33776e649a01192f80f0203
Olivier Butler <olivier.butler@gmail.com>
2017-09-11 20:27:16 -0700

    Can now dynamically add to the list (non-persisting data).

--- Beat List/ViewController.swift
+++ Beat List/ViewController.swift
@@ -5,13 +5,39 @@
 //  Created by Olivier Butler on 11/09/2017.
 //  Copyright © 2017 Olivier Butler. All rights reserved.
 //
-
+import Foundation
 import UIKit
 
-class ViewController: UIViewController {
+extension ViewController: UITableViewDataSource {
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return funArray.count
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: "MyCell", for: indexPath)
+        cell.textLabel?.text = funArray[indexPath.row]
+        return cell
+    }
+    
+    
+}
 
+class ViewController: UIViewController {
+    var funArray = ["Jamie sucks eggs", "A lone human stalks a bear", "Many feet have a army", "Feathers are light", "I like long roads"]
+    @IBOutlet weak var masterTable: UITableView!
+    @IBOutlet weak var taskField: UITextField!
+    @IBAction func beatButtonPress(_ sender: UIButton) {
+        let maybeContent = taskField.text
+        if let content:String = maybeContent {
+            funArray.append(content)
+            taskField.text = ""
+            masterTable.reloadData()
+        }
+    }
+    
     override func viewDidLoad() {
         super.viewDidLoad()
+        masterTable.dataSource = self
         // Do any additional setup after loading the view, typically from a nib.
     }
 
